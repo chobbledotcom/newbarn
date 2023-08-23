c7a29db8f4578f8cf14a7fcb8a57fe957a6592c6
Stefan <git@stefn.co.uk>
2023-08-23 11:03:09 +0100

    auto-updated after build

--- build.sh
+++ build.sh
@@ -8,8 +8,12 @@ bundle exec jekyll build
 html-minifier --input-dir _site --output-dir _site --file-ext html --collapse-whitespace --minify-css --remove-comments --remove-attribute-quotes --remove-redundant-attributes
 
 set +x
-export NEOCITIES_API_KEY=$(cat /home/$1/.neocities/newbarn)
+export NEOCITIES_API_KEY=$(cat /home/user/.neocities/newbarn)
 set -x
 
 gem install --no-document neocities
 neocities push --prune _site
+
+git add .
+git commit -m "auto-updated after build"
+git push
