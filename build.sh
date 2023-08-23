8c6897b92790d9b33c17942fb19022f43c7ef625
Stefan <git@stefn.co.uk>
2023-08-23 01:48:13 +0100

    fix links

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env
+
+bundle config jobs 4
+bundle install
+export JEKYLL_ENV=production
+bundle exec jekyll build
+
+html-minifier --input-dir _site --output-dir _site --file-ext html --collapse-whitespace --minify-css --remove-comments --remove-attribute-quotes --remove-redundant-attributes
+
+set +x
+export NEOCITIES_API_KEY=$(cat /home/$1/.neocities/newbarn)
+set -x
+
+gem install --no-document neocities
+neocities push --prune _site
