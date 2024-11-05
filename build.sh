5d89de891627bed0df65b2042e6df423d2086cd6
Stefan <git@stefn.co.uk>
2024-11-05 15:58:36 +0000

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env
 
-bundle config jobs 4
-bundle install
 export JEKYLL_ENV=production
-bundle exec jekyll build
-
-npx --yes @divriots/jampack ./_site
-
-html-minifier --input-dir _site --output-dir _site --file-ext html --collapse-whitespace --minify-css --remove-comments --remove-attribute-quotes --remove-redundant-attributes
-
 set +x
 export NEOCITIES_API_KEY=$(cat /home/user/.neocities/newbarn)
 set -x
-
-gem install --no-document neocities
+rm -rf _site
+bundle exec jekyll build
+html-minifier --input-dir _site --output-dir _site --collapse-whitespace --file-ext html
 neocities push --prune _site
-
-git add .
-git commit -m "auto-updated after build"
-git push
--- shell.nix
+++ shell.nix
@@ -2,19 +2,11 @@ with (import <nixpkgs> {});
 mkShell {
   buildInputs = [
     bundler
-    ruby
+    html-minifier
+    lightningcss
+    neocities-cli
     nodejs
-    nodePackages_latest.npm
-    nodePackages_latest.html-minifier
+    nodePackages.pnpm
+    sass
   ];
-  shellHook = ''
-    mkdir -p .nix-gems
-
-    export GEM_HOME=$PWD/.nix-gems
-    export GEM_PATH=$GEM_HOME
-    export PATH=$GEM_HOME/bin:$PATH
-    export PATH=$PWD/bin:$PATH
-
-    gem list -i ^neocities$ || gem install neocities --no-document
-  '';
 }
