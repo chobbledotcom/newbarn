00b861676eef7de2585852360266d8f17abb6fa5
Stefan <git@stefn.co.uk>
2024-11-05 16:07:09 +0000

    Add bundle install

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set +x
 export NEOCITIES_API_KEY=$(cat ~/.neocities/newbarn)
 set -x
 rm -rf _site
+bundle install
 bundle exec jekyll build
 html-minifier --input-dir _site --output-dir _site --collapse-whitespace --file-ext html
 neocities push --prune _site
