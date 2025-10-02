5d2425062ac58c3582a01c2da2d5cee943ee3a98
Stefan <git@stefn.co.uk>
2025-10-02 21:36:38 +0100

    Concise flake

--- flake.nix
+++ flake.nix
@@ -1,118 +1,76 @@
 {
   description = "New Barn Ltd website";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs";
-  };
+  inputs.nixpkgs.url = "nixpkgs";
 
   outputs = { self, nixpkgs }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
+      siteName = "newbarnltd-co-uk";
+
+      makeRubyEnv = pkgs: pkgs.bundlerEnv {
+        name = siteName;
+        ruby = pkgs.ruby_3_3;
+        gemfile = ./Gemfile;
+        lockfile = ./Gemfile.lock;
+        gemset = ./gemset.nix;
+      };
     in
     {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          rubyEnv = makeRubyEnv pkgs;
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = siteName;
+            src = pkgs.lib.cleanSource ./.;
 
-        rubyEnv = pkgs.bundlerEnv {
-          name = "newbarnltd-co-uk";
-          ruby = pkgs.ruby_3_3;
-          gemfile = ./Gemfile;
-          lockfile = ./Gemfile.lock;
-          gemset = ./gemset.nix;
-        };
+            nativeBuildInputs = with pkgs; [ ruby_3_3 minify ];
 
-        site = pkgs.stdenv.mkDerivation {
-          name = "newbarnltd-co-uk";
+            configurePhase = ''
+              export HOME=$TMPDIR
+              mkdir -p _site
+            '';
 
-          src = pkgs.lib.cleanSource ./.;
+            buildPhase = ''
+              echo "Building site with Jekyll..."
+              JEKYLL_ENV=production ${rubyEnv}/bin/jekyll build --source . --destination _site --trace
 
-          nativeBuildInputs = with pkgs; [
-            ruby_3_3
-            minify
-          ];
+              echo 'Minifying HTML'
+              minify --all --recursive --output . _site
+            '';
 
-          configurePhase = ''
-            export HOME=$TMPDIR
-            mkdir -p _site
-          '';
-
-          buildPhase = ''
-            echo "Building site with Jekyll..."
-            JEKYLL_ENV=production ${rubyEnv}/bin/jekyll build --source . --destination _site --trace
-
-            echo 'Minifying HTML'
-            minify --all --recursive --output . _site
-          '';
-
-          installPhase = ''
-            echo "Creating output directory..."
-            mkdir -p $out
-
-            echo "Copying site files..."
-            cp -r _site/* $out/
-          '';
-        };
-      in
-      {
-        default = site;
-      });
+            installPhase = ''
+              mkdir -p $out
+              cp -r _site/* $out/
+            '';
+          };
+        });
 
       devShells = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-
-          rubyEnv = pkgs.bundlerEnv {
-            name = "newbarnltd-co-uk";
-            ruby = pkgs.ruby_3_3;
-            gemfile = ./Gemfile;
-            lockfile = ./Gemfile.lock;
-            gemset = ./gemset.nix;
-          };
+          rubyEnv = makeRubyEnv pkgs;
         in
         {
           default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rubyEnv
-            ruby_3_3
-            rubyPackages_3_3.ffi
-            libffi
-          ];
+            buildInputs = with pkgs; [ rubyEnv ruby_3_3 rubyPackages_3_3.ffi libffi ];
 
-          shellHook = ''
-            serve() {
-              ${rubyEnv}/bin/jekyll serve --watch &
-              JEKYLL_PID=$!
-
-              cleanup_serve() {
-                echo "Cleaning up serve process..."
-                kill $JEKYLL_PID 2>/dev/null
-                wait $JEKYLL_PID 2>/dev/null
+            shellHook = ''
+              serve() {
+                ${rubyEnv}/bin/jekyll serve --watch &
+                JEKYLL_PID=$!
+                trap "kill $JEKYLL_PID 2>/dev/null; wait $JEKYLL_PID 2>/dev/null" EXIT INT TERM
+                wait $JEKYLL_PID
+                trap - EXIT INT TERM
               }
-
-              trap cleanup_serve EXIT INT TERM
-
-              wait $JEKYLL_PID
-
-              cleanup_serve
-
-              trap - EXIT INT TERM
-            }
-
-            export -f serve
-
-            cleanup() {
-              echo "Cleaning up..."
-              rm -rf _site .jekyll-cache .jekyll-metadata
-            }
-
-            trap cleanup EXIT
-
-            echo "Development environment ready!"
-            echo "Run 'serve' to start development server"
-          '';
-        };
-      });
+              export -f serve
+              echo "Development environment ready! Run 'serve' to start development server"
+            '';
+          };
+        });
     };
 }
